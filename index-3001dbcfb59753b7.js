0201dc2987d9b8804f2b4aa925fcb86d121dfbbb
Evan Chang <evanc577@gmail.com>
2022-05-10 00:48:02 -0400

    Update gh-pages

--- index-3001dbcfb59753b7.js
+++ index-3001dbcfb59753b7.js
@@ -298,7 +298,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-19424b31d4e4072b_bg.wasm', import.meta.url);
+        input = new URL('index-3001dbcfb59753b7_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -639,12 +639,12 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper799 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 270, __wbg_adapter_22);
+    imports.wbg.__wbindgen_closure_wrapper808 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 275, __wbg_adapter_22);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper934 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 313, __wbg_adapter_25);
+    imports.wbg.__wbindgen_closure_wrapper943 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 318, __wbg_adapter_25);
         return addHeapObject(ret);
     };
 
