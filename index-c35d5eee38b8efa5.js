7a4afc2d69a16d7085a55d80e9327c33c4b8bbda
Evan Chang <evanc577@gmail.com>
2022-05-13 22:54:42 -0400

    Update gh-pages

--- index-c35d5eee38b8efa5.js
+++ index-c35d5eee38b8efa5.js
@@ -298,7 +298,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-c32f6e2e6466a494_bg.wasm', import.meta.url);
+        input = new URL('index-c35d5eee38b8efa5_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
