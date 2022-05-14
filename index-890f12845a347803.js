ff0bde18ef7cc1a7ec60ce5b2d08cebda66d19a4
Evan Chang <evanc577@gmail.com>
2022-05-13 23:34:21 -0400

    Update gh-pages

--- index-890f12845a347803.js
+++ index-890f12845a347803.js
@@ -298,7 +298,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-c35d5eee38b8efa5_bg.wasm', import.meta.url);
+        input = new URL('index-890f12845a347803_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
